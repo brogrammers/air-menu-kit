6a7bcc411c2e45029d1ca71930dff9a83ee7c313
robertlis <robert.lis2@mail.dcu.ie>
2014-02-02 19:20:54 +0000

    Another attempt at cocoapods

---
+++ Project/AirMenuKit/Classes/AirMenuClient.m
@@ -0,0 +1,17 @@
+//
+//  AirMenuClient.m
+//  AirMenuKit
+//
+//  Created by Robert Lis on 02/02/2014.
+//  Copyright (c) 2014 air-menu. All rights reserved.
+//
+
+#import "AirMenuClient.h"
+
+@implementation AirMenuClient
++(void)saySomething
+{
+    NSLog(@"Blah");
+}
+
+@end
