5755e3d5e2e8d01b1d4b246295e53511999866fc
robertlis <robert.lis2@mail.dcu.ie>
2014-02-03 14:32:32 +0000

    spec test

---
+++ Classes/Networking/AirMenuClient.h
@@ -0,0 +1,13 @@
+//
+//  AirMenuClient.h
+//  AirMenuKit
+//
+//  Created by Robert Lis on 03/02/2014.
+//  Copyright (c) 2014 air-menu. All rights reserved.
+//
+
+#import "AFHTTPSessionManager.h"
+
+@interface AirMenuClient : AFHTTPSessionManager
+
+@end
---
+++ Classes/Networking/AirMenuClient.m
@@ -0,0 +1,13 @@
+//
+//  AirMenuClient.m
+//  AirMenuKit
+//
+//  Created by Robert Lis on 03/02/2014.
+//  Copyright (c) 2014 air-menu. All rights reserved.
+//
+
+#import "AirMenuClient.h"
+
+@implementation AirMenuClient
+
+@end
