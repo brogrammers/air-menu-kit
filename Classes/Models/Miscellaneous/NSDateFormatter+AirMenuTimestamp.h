2ab895dd795bcd6c58c777aa1c97c7b2a80371ca
robertlis <robert.lis2@mail.dcu.ie>
2014-02-04 21:55:19 +0000

    Moved NSDateFormatter

---
+++ Classes/Models/Miscellaneous/NSDateFormatter+AirMenuTimestamp.h
@@ -0,0 +1,13 @@
+//
+//  NSDateFormatter+AirMenuTimestamp.h
+//  AirMenuKit
+//
+//  Created by Robert Lis on 04/02/2014.
+//  Copyright (c) 2014 air-menu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDateFormatter (AirMenuTimestamp)
++ (NSDateFormatter *)sharedAirMenuFormatter;
+@end
