9fe6606f5bac1fa4f6508d3c9eea28b83d9258f8
robertlis <robert.lis2@mail.dcu.ie>
2014-02-06 21:33:32 +0000

    added test case for AMRestaurant

---
+++ Project/AirMenuKitTests/AMRestaurantTests.m
@@ -0,0 +1,31 @@
+//
+//  AMMenuSectionTests.m
+//  AirMenuKit
+//
+//  Created by Robert Lis on 04/02/2014.
+//  Copyright (c) 2014 air-menu. All rights reserved.
+//
+
+#import <Kiwi/Kiwi.h>
+#import <objc/message.h>
+#import "AMMenu.h"
+#import "NSDateFormatter+AirMenuTimestamp.h"
+
+
+SPEC_BEGIN(AMRestaurantTests)
+
+describe(@"AMMenu", ^{
+    context(@"object", ^{
+       
+    });
+    
+    context(@"class", ^{
+        
+    });
+    
+    context(@"mapping", ^{
+        
+    });
+});
+
+SPEC_END
