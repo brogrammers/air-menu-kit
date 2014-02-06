237c01b0920dffdc3421533523660f858fd1d519
robertlis <robert.lis2@mail.dcu.ie>
2014-02-06 21:30:29 +0000

    Finished implementation of a menu. Added transformers and mappings plus tests.

--- Classes/Models/AMMenu.h
+++ Classes/Models/AMMenu.h
@@ -9,6 +9,10 @@
 #import <Foundation/Foundation.h>
 #import <Mantle/Mantle.h>
 
-@interface AMMenu : MTLModel
-
+@interface AMMenu : MTLModel <MTLJSONSerializing>
+@property (nonatomic, strong, readonly) NSString *identifier;
+@property (nonatomic, strong, readonly) NSDate *createdAt;
+@property (nonatomic, strong, readonly) NSDate *updatedAt;
+@property (nonatomic, strong, readonly) NSString *name;
+@property (nonatomic, strong, readonly) NSArray *menuSections;
 @end
--- Classes/Models/AMMenu.m
+++ Classes/Models/AMMenu.m
@@ -7,7 +7,43 @@
 //
 
 #import "AMMenu.h"
+#import "AMMenuSection.h"
+#import "NSDateFormatter+AirMenuTimestamp.h"
 
 @implementation AMMenu
+
+#pragma mark - Mantle Framework mapping hooks
+
++(NSDictionary *)JSONKeyPathsByPropertyKey
+{
+    return @{@"identifier" : @"id",
+             @"createdAt" : @"created_at",
+             @"updatedAt" : @"updated_at",
+             @"name" : @"name",
+             @"menuSections": @"menu_sections"};
+}
+
++(NSValueTransformer *)createdAtJSONTransformer
+{
+    return [MTLValueTransformer reversibleTransformerWithForwardBlock:^(NSString *str) {
+        return [[NSDateFormatter sharedAirMenuFormatter] dateFromString:str];
+    } reverseBlock:^(NSDate *date) {
+        return [[NSDateFormatter sharedAirMenuFormatter] stringFromDate:date];
+    }];
+}
+
++(NSValueTransformer *)updatedAtJSONTransformer
+{
+    return [MTLValueTransformer reversibleTransformerWithForwardBlock:^(NSString *str) {
+        return [[NSDateFormatter sharedAirMenuFormatter] dateFromString:str];
+    } reverseBlock:^(NSDate *date) {
+        return [[NSDateFormatter sharedAirMenuFormatter] stringFromDate:date];
+    }];
+}
+
++(NSValueTransformer *)menuSectionsJSONTransformer
+{
+    return [NSValueTransformer mtl_JSONArrayTransformerWithModelClass:[AMMenuSection class]];
+}
 
 @end
