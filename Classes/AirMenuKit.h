694e886a0aa5b260da5ddb2b466ee62fccdde60b
robertlis <robert.lis2@mail.dcu.ie>
2014-03-19 23:51:43 +0000

    Added global import for the framework and added relevant headers to public headers.

---
+++ Classes/AirMenuKit.h
@@ -0,0 +1,28 @@
+//
+//  AirMenuKit.h
+//  AirMenuKit
+//
+//  Created by Robert Lis on 19/03/2014.
+//  Copyright (c) 2014 air-menu. All rights reserved.
+//
+
+
+//networking
+#import "AMClient.h"
+#import "AMOAuthToken.h"
+#import "AMOAuthApplication.h"
+#import "AMClient+Company.h"
+#import "AMClient+Restaurant.h"
+#import "AMClient+Menu.h"
+#import "AMClient+MenuSection.h"
+#import "AMClient+MenuItem.h"
+#import "AMClient+User.h"
+
+//models
+#import "AMCompany.h"
+#import "AMRestaurant.h"
+#import "AMMenu.h"
+#import "AMMenuSection.h"
+#import "AMMenuItem.h"
+#import "AMUser.h"
+#import "AMAddress.h"
