ccf869b890e2b56b41987b13983f4b9d86e62959
robertlis <robert.lis2@mail.dcu.ie>
2014-04-13 22:02:31 +0100

    added order item properties and their tests

--- Classes/Models/AMOrderItem.h
+++ Classes/Models/AMOrderItem.h
@@ -7,7 +7,19 @@
 //
 
 #import <Mantle/Mantle.h>
+#import "AMOrder.h"
+#import "AMMenuItem.h"
 
 @interface AMOrderItem : MTLModel <MTLJSONSerializing>
-
+@property (nonatomic, readonly, strong) NSNumber *identifier;
+@property (nonatomic, readonly, strong) NSString *comment;
+@property (nonatomic, readonly, strong) NSNumber *count;
+@property (nonatomic, readonly, strong) NSString *state;
+@property (nonatomic, readonly, strong) NSDate *approvedAt;
+@property (nonatomic, readonly, strong) NSDate *declinedAt;
+@property (nonatomic, readonly, strong) NSDate *prepareTimeStart;
+@property (nonatomic, readonly, strong) NSDate *prepareTimeEnd;
+@property (nonatomic, readonly, strong) NSDate *servedAt;
+@property (nonatomic, readonly, strong) AMOrder *order;
+@property (nonatomic, readonly, strong) AMMenuItem *menuItem;
 @end
--- Project/AirMenuKitTests/Models/AMOrderItemTests.m
+++ Project/AirMenuKitTests/Models/AMOrderItemTests.m
@@ -8,11 +8,78 @@
 
 #import <Kiwi/Kiwi.h>
 #import <Mantle/Mantle.h>
+#import "AMOrderItem.h"
 
 SPEC_BEGIN(AMOrderItemTests)
 describe(@"AMOrderItem", ^{
     context(@"object", ^{
+        __block AMOrderItem *item;
+        beforeAll(^{
+            item = [[AMOrderItem alloc] init];
+            [item setValuesForKeysWithDictionary:@{@"identifier" : @1,
+                                                   @"comment" : @"a comment",
+                                                   @"count" : @1,
+                                                   @"state" : @"new",
+                                                   @"approvedAt" : [NSDate dateWithTimeIntervalSince1970:1],
+                                                   @"declinedAt" : [NSDate dateWithTimeIntervalSince1970:1],
+                                                   @"prepareTimeStart" : [NSDate dateWithTimeIntervalSince1970:1],
+                                                   @"prepareTimeEnd" : [NSDate dateWithTimeIntervalSince1970:1],
+                                                   @"servedAt" : [NSDate dateWithTimeIntervalSince1970:1],
+                                                   @"order" : [AMOrder new],
+                                                   @"menuItem" : [AMMenuItem new]}];
+        });
         
+        it(@"subclasses MTLModel", ^{
+            [[item should] beKindOfClass:[MTLModel class]];
+        });
+        
+        it(@"conforms to MTLJSONSerializing attribute", ^{
+            [[item should] conformToProtocol:@protocol(MTLJSONSerializing)];
+        });
+        
+        it(@"has identifier attribute", ^{
+            [[item.identifier should] equal:@1];
+        });
+        
+        it(@"has comment attribute", ^{
+            [[item.comment should] equal:@"a comment"];
+        });
+        
+        it(@"has count attribute", ^{
+            [[item.count should] equal:@1];
+        });
+        
+        it(@"has state attribute", ^{
+            [[item.state should] equal:@"new"];
+        });
+        
+        it(@"has approvedAt attribute", ^{
+            [[item.approvedAt should] equal:[NSDate dateWithTimeIntervalSince1970:1]];
+        });
+        
+        it(@"has declinedAt attribute", ^{
+            [[item.declinedAt should] equal:[NSDate dateWithTimeIntervalSince1970:1]];
+        });
+        
+        it(@"has prepareTimeStart attribute", ^{
+            [[item.prepareTimeStart should] equal:[NSDate dateWithTimeIntervalSince1970:1]];
+        });
+        
+        it(@"has prepareTimeEnd attribute", ^{
+            [[item.prepareTimeEnd should] equal:[NSDate dateWithTimeIntervalSince1970:1]];
+        });
+        
+        it(@"has servedAt attribute", ^{
+            [[item.servedAt should] equal:[NSDate dateWithTimeIntervalSince1970:1]];
+        });
+        
+        it(@"has order attribute", ^{
+            [[item.order should] equal:[AMOrder new]];
+        });
+        
+        it(@"has menu item attribute", ^{
+            [[item.menuItem should] equal:[AMMenuItem new]];
+        });
     });
     
     context(@"class", ^{
